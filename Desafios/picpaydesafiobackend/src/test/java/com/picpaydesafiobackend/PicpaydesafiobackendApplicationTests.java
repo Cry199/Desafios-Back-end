f3c189abb3cfe199ff5b46ae6020fa7b76bf4fc2
Cauã <caualeu2016@gmail.com>
2023-10-17 16:58:40 -0300

    Start of the PicPay Challenge Home - v0.0.1

---
+++ Desafios/picpaydesafiobackend/src/test/java/com/picpaydesafiobackend/PicpaydesafiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaydesafiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
