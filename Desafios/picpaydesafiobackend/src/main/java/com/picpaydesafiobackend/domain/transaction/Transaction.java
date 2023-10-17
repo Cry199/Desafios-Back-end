2d1532fbf6050df8ce580b4b23ff12907bd53da0
Cauã <caualeu2016@gmail.com>
2023-10-17 17:03:48 -0300

    create Transaction Entity - v0.0.3

---
+++ Desafios/picpaydesafiobackend/src/main/java/com/picpaydesafiobackend/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.picpaydesafiobackend.domain.transaction;
+
+import com.picpaydesafiobackend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal anount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
