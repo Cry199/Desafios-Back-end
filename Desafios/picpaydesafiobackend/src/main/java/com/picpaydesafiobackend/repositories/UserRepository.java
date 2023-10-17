3ffcc5da80e80bee78de2125fdfee2339bc08bc6
Cauã <caualeu2016@gmail.com>
2023-10-17 17:04:31 -0300

    create UserRepository

---
+++ Desafios/picpaydesafiobackend/src/main/java/com/picpaydesafiobackend/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.picpaydesafiobackend.repositories;
+
+import com.picpaydesafiobackend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+
+}
