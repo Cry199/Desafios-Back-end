7a6b9c6d731ab2d133e38f8e45dbf3b36b5bcdd3
Cauã <caualeu2016@gmail.com>
2023-10-18 10:22:16 -0300

    Create TransactionRepository - v0.0.4

---
+++ Desafios/picpaydesafiobackend/src/main/java/com/picpaydesafiobackend/repositories/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.picpaydesafiobackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<TransactionRepository, Long>
+{
+    
+}
